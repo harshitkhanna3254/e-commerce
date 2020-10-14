65e23c4f468ab363510c8222d0179e35306e9189
harshitkhanna3254 <harshitkhanna3254@gmail.com>
2020-10-14 19:47:49 +0530

    showed images from static js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to HendrixMart</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
